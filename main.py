019cd8d50ef531e3ccd4cb9aeb4da14a77e89340
Steve <stevenegeer@gmail.com>
2023-10-20 15:04:18 -0700

    Created PaddleManager and Paddle classes

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
 import pygame
-from typing import Tuple
 from pygame.locals import *
+import objects.paddle as p
 
 clock = pygame.time.Clock()
-
-def render_paddle(height: int) -> None:
-  paddle = pygame.Surface((20, height))
-  paddle.fill((255, 255, 255))
-
-  return paddle
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((720, 500))
   pygame.display.set_caption("Basic pygame")
 
-  screen_height = screen.get_height()
-  paddle_height = screen.get_height() / 4
-
   bg = pygame.Surface(screen.get_size()).convert()
   bg.fill((30, 30, 30))
 
-  p2 = render_paddle(paddle_height)
-  p1 = render_paddle(paddle_height)
+  mgr = p.PaddleManager(screen)
+  mgr.draw()
 
-  screen.blit(p1, (0, 0))
-  screen.blit(p2, (screen.get_width() - 25, 0))
   screen.blit(bg, (0,0))
-
   pygame.display.flip()
 
-  cur_y = 0
-
-  run = True
-  while run:
+  while True:
     clock.tick(60)
     for event in pygame.event.get():
       if event.type == QUIT:
         return
 
-    keys = pygame.key.get_pressed()
-    if keys[K_DOWN]:
-      cur_y += 10
-    if keys[K_UP]:
-      cur_y -= 10
-
-    if cur_y <= 0:
-      cur_y = 0
-
-    if cur_y >= screen_height - paddle_height:
-      cur_y = screen_height - paddle_height
-
     screen.blit(bg, (0, 0))
-    screen.blit(p1, (0, cur_y))
-    screen.blit(p2, (screen.get_width() - 25, 0))
+
+    mgr.listen_for_keys()
+    mgr.draw()
+
     pygame.display.flip()
 
 if __name__ == '__main__':
--- objects/paddle.py
+++ objects/paddle.py
@@ -1,5 +1,55 @@
-from pygame import Surface
+import pygame
+from typing import List, Tuple
+from pygame.locals import K_DOWN, K_UP
 
-class Paddle(Surface):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+# Increase this to make the paddle smaller
+PADDLE_SIZE_DIVISOR = 4
+PADDLE_WIDTH = 20
+
+class Paddle():
+  srf: pygame.Surface
+  x: int
+  y: int
+
+  def __init__(self, size: Tuple[int, int], x: int):
+    self.srf = pygame.Surface(size)
+    self.srf.fill((255, 255, 255))
+    self.srf.convert()
+
+    self.x = x
+    self.y = 0
+
+  @property
+  def position(self) -> Tuple[int, int]:
+    return (self.x, self.y)
+
+class PaddleManager():
+  paddles: List[Paddle]
+  screen: pygame.Surface
+  max_y: int
+
+  def __init__(self, screen: pygame.Surface):
+    PADDLE_HEIGHT = screen.get_height() / PADDLE_SIZE_DIVISOR
+
+    self.screen = screen
+    self.max_y = self.screen.get_height() - PADDLE_HEIGHT
+
+    p1 = Paddle((PADDLE_WIDTH, PADDLE_HEIGHT), 0)
+    p2 = Paddle((PADDLE_WIDTH, PADDLE_HEIGHT), self.screen.get_width() - PADDLE_WIDTH)
+
+    self.paddles = [p1, p2]
+
+  def draw(self):
+    for p in self.paddles:
+      self.screen.blit(p.srf, p.position)
+
+  def listen_for_keys(self) -> None:
+    keys = pygame.key.get_pressed()
+    p1, _ = self.paddles
+
+    if keys[K_DOWN]:
+      if p1.y <= self.max_y:
+        p1.y += 10
+    if keys[K_UP]:
+      if p1.y >= 0:
+        p1.y -= 10
